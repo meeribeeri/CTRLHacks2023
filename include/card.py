a8478c46de68d91d702c0072b16cec4901289815
Alex Ngo <104698795+meeribeeri@users.noreply.github.com>
2023-09-22 20:40:43 +0000

    Co-authored-by: llnotion <llnotion@users.noreply.github.com>

---
+++ include/card.py
@@ -0,0 +1,12 @@
+import tkinter
+
+class Card():
+    def __init__(self):
+        pass
+
+    def use(self):
+        pass
+
+class MathCard(Card):
+    def __init__(self):
+        pass
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import tkinter
+import include.card
 
 def main():
     pass
 
 
+if __name__ == "main":
+    main()
