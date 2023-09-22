bd4641b734bcf015742e05660d3d1d08ea8739e7
Meeribeeri <starstrike3406@gmail.com>
2023-09-21 22:22:17 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import discord
 import discord.ext
+from discord.ext import commands
 
-# setting up the bot
 intents = discord.Intents.all() 
-# if you don't want all intents you can do discord.Intents.default()
-client = discord.Client(intents=intents)
-tree = discord.app_commands.CommandTree(client)
+#client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix = "+",intents=discord.Intents.all())
+#tree = discord.app_commands.CommandTree(client)
 guild = discord.Object(id=1154614970676412446)
 
 # sync the slash command to your server
 @client.event
 async def on_ready():
-    await tree.sync(guild=guild)
-    # print "ready" in the console when the bot is ready to work
+    await client.tree.sync(guild=guild)
     print("ready")
 
+@client.command(aliases = ["sync"])
+async def synchronise(ctx):
+    print("synchronising")
+    await client.tree.sync(guild=guild)
+    await ctx.send("Synchonised!")
+    print("synced")
+
 # make the slash command
-@tree.command(name="name", description="description")
-async def slash_command(interaction: discord.Interaction):    
+@client.tree.command(name="name", description="description")
+async def slash_command(interaction: discord.Interaction, test : discord.app_commands.Range[int,0,100]):    
     await interaction.response.send_message("command")
 
 client.run('MTE1NDYxMjQ5ODAyMDk2MjMyNA.GAggS6.hEG38XxfI0VfglZ3RSujbeSBjNpAn0AspBxQn4')
