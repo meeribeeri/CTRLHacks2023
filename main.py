5124884afa2818eae990246971a14d29249dbf87
Meeribeeri <starstrike3406@gmail.com>
2023-09-21 21:43:47 -0600

    new file:   .env 	modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import discord
+import discord.ext
 
-def main():
-    pass
+# setting up the bot
+intents = discord.Intents.all() 
+# if you don't want all intents you can do discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)
+guild = discord.Object(id=1154614970676412446)
 
-if "__name__" == "main":
-    main()
+# sync the slash command to your server
+@client.event
+async def on_ready():
+    await tree.sync(guild=guild)
+    # print "ready" in the console when the bot is ready to work
+    print("ready")
+
+# make the slash command
+@tree.command(name="name", description="description")
+async def slash_command(interaction: discord.Interaction):    
+    await interaction.response.send_message("command")
+
+client.run('MTE1NDYxMjQ5ODAyMDk2MjMyNA.GAggS6.hEG38XxfI0VfglZ3RSujbeSBjNpAn0AspBxQn4')
