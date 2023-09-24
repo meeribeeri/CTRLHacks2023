eb5b3df7441c75c255effd5e34094919a499bfe9
Meeribeeri <starstrike3406@gmail.com>
2023-09-23 21:08:14 -0600

    modified:   include/__pycache__/misc.cpython-311.pyc 	modified:   include/__pycache__/player.cpython-311.pyc 	modified:   include/misc.py 	modified:   include/player.py 	modified:   main.py

--- include/player.py
+++ include/player.py
@@ -23,7 +23,25 @@ class Player():
         self.frame.grid_rowconfigure(2, weight=1)
         self.frame.grid_columnconfigure(0, weight=1)
 
-        
+        self.side_label = Label(master=self.frame,text="Cards:")
+        self.side_label.grid(row=0,column=0,columnspan=2,sticky=N+S)
+
+        self.frame_components = []
+
+        while True:
+            if len(self.hand) < 5:
+                cardDrawn = deck.pop(randint(0,len(deck)-1))
+                self.hand.append(cardDrawn)
+            else:
+                break
+
+        for i in range(0,5):
+            self.frame_components.append(Label(master=self.frame,text=self.hand[i].text,image=self.hand[i].image,compound=BOTTOM))
+            self.frame_components[i*2].grid(row=1,column=i+1,sticky=N)
+            self.frame_components.append(Checkbutton(master=self.frame,text="Play",onvalue=True,offvalue=False,variable=self.cards_to_play[i]))
+            self.frame_components[i*2+1].grid(row=2,column=i+1,sticky=N)
+        self.frame_components.append(Button(master=self.frame,text="Play Cards",command=self.endCardChoice))
+        self.frame_components[10].grid(row=3,column=0,columnspan=6,sticky=N)
             
     def damage(self,damage : int):
         self.hp-=damage
@@ -45,18 +63,34 @@ class Player():
         self.question_area.addQuestion(card_used,rowsdown)
 
     def turnStart(self):
+        self.activate()
         self.frame.grid(column=0,row=8,columnspan=6,rowspan=3,sticky=N)
-        self.side_label = Label(master=self.frame,text="Cards:")
-        self.side_label.grid(row=0,column=0,columnspan=2,sticky=N+S)
+        for child in self.frame.winfo_children():
+            child.configure(state='normal') #turns each component of frame on, so that users can see it
+        
+
+
+    def endCardChoice(self):
+        for child in self.frame.winfo_children():
+            child.configure(state='disable') #turns each component off
+        for i in range(0,5):
+            match self.cards_to_play[i].get():
+                case True:
+                    self.play(card_index=i,rowsdown=i)
+        self.draw()
+
+    def turnEnd(self):
+        self.cards_to_play = [BooleanVar(),BooleanVar(),BooleanVar(),BooleanVar(),BooleanVar()]
+        for child in self.frame.winfo_children():
+            child.configure(state='disable')
+
+        for component in self.frame_components:
+            component.destroy()
 
         self.frame_components = []
 
-        while True:
-            if len(self.hand) < 5:
-                cardDrawn = self.deck.pop(randint(0,len(self.deck)-1))
-                self.hand.append(cardDrawn)
-            else:
-                break
+    def activate(self):
+        
 
         for i in range(0,5):
             self.frame_components.append(Label(master=self.frame,text=self.hand[i].text,image=self.hand[i].image,compound=BOTTOM))
@@ -65,17 +99,3 @@ class Player():
             self.frame_components[i*2+1].grid(row=2,column=i+1,sticky=N)
         self.frame_components.append(Button(master=self.frame,text="Play Cards",command=self.endCardChoice))
         self.frame_components[10].grid(row=3,column=0,columnspan=6,sticky=N)
-
-        
-
-    def endCardChoice(self):
-        for child in self.frame.winfo_children():
-            child.destroy()
-        self.frame_components = []
-        for i in range(0,5):
-            match self.cards_to_play[i].get():
-                case True:
-                    self.play(card_index=i,rowsdown=i)
-        self.draw()
-
-        
--- main.py
+++ main.py
@@ -12,8 +12,6 @@ from include.misc import *
 
 def main():
     Win = Tk()
-    game_state = 0
-
 
     Win.title('Card Learning Game') #self explanitory
 
@@ -23,12 +21,6 @@ def main():
     Win.iconbitmap('') #Window Icon
     Win.grid_rowconfigure(0, weight=1)
     Win.grid_columnconfigure(0, weight=1)
-
-    Ma=Menubutton(Win,text = 'Math') #This is the button for math!
-    #add anything you need in here such as shape,geometry,mesh,etc
-    #Ma.grid(column=0, row=0)#instead of using x,y coordinates i opted for colums and rows!
-    def damage(damage : int):
-        print(damage, "K")
     
     top_bar = TopBar(Win)
 
@@ -40,10 +32,10 @@ def main():
     Player_1 = Player(deck=deck,window=Win,question_area=question_frame)
     Player_2 = Player(deck=deck,window=Win,question_area=question_frame)
     question_frame.targets = [Player_1,Player_2]
+
     question_frame.retarget(1)
     Player_1.turnStart()
     
-    print(game_state)
     Win.mainloop() #self explanitory
 #anything below here won't run, at least within the function
 
